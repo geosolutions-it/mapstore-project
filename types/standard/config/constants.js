16c0b5fbfe8e56fa75182c59187fa9a7a0826e24
mv88 <matteo.velludini.geosolutions@gmail.com>
2020-12-04 19:42:35 +0100

    clean up for constants

--- types/standard/config/constants.js
+++ types/standard/config/constants.js
@@ -1,22 +1,9 @@
-const LOADING_MESSAGE = "Loading MapStore";
-const LOADING_MESSAGE_INDEX = LOADING_MESSAGE;
-const LOADING_MESSAGE_EMBEDDED = LOADING_MESSAGE;
-
-
-const TITLE = "MapStore";
-const TITLE_INDEX = TITLE;
-const TITLE_EMBEDDED = TITLE;
-const TITLE_API = "Page with MapStore API";
-
-
 module.exports = {
-    title: "MapStore",
     titleUnsupported: "Unsupported mapstore browser",
-    titleIndex: TITLE_INDEX,
-    titleEmbedded: TITLE_EMBEDDED,
-    titleApi: TITLE_API,
+    titleIndex: "MapStore",
+    titleEmbedded: "MapStore",
+    titleApi: "Page with MapStore API",
     favicon: "img/favicon.ico",
-    loadingMessage: LOADING_MESSAGE,
-    loadingMessageIndex: LOADING_MESSAGE_INDEX,
-    loadingMessageEmbedded: LOADING_MESSAGE_EMBEDDED
+    loadingMessageIndex: "Loading MapStore",
+    loadingMessageEmbedded: "Loading MapStore"
 }
